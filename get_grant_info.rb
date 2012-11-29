02f6599ce80daa66f437ae7d1fa6ea52453711a9
M. Demas <demas.ma@gmail.com>
2012-11-29 16:06:54 -0500

    added testing bit

--- get_grant_info.rb
+++ get_grant_info.rb
@@ -14,6 +14,7 @@ def get_grant_info(pmid)
   # assemble the url (standard pubmed format)
   base_url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
   url = base_url + "efetch.fcgi?db=pubmed&id=#{pmid}&retmode=xml"
+  puts url
   doc = Nokogiri::XML(open(url))
   doc.xpath('///Grant').each do |node|
     puts "grant info is:\n\t#{node.text}"
