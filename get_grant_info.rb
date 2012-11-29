34fe78cb2e170f7e6c5369e27b2b961ca5497760
M. Demas <demas.ma@gmail.com>
2012-11-29 16:18:54 -0500

    solved command line arg issue

--- get_grant_info.rb
+++ get_grant_info.rb
@@ -12,16 +12,20 @@ require 'open-uri'
 
 def get_grant_info(pmid)
   # assemble the url (standard pubmed format)
-  base_url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
-  url = base_url + "efetch.fcgi?db=pubmed&id=#{pmid}&retmode=xml"
-  puts url
-  doc = Nokogiri::XML(open(url))
-  doc.xpath('///Grant').each do |node|
-    puts "grant info is:\n\t#{node.text}"
+  if !pmid.nil?
+    base_url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+    url = base_url + "efetch.fcgi?db=pubmed&id=#{pmid}&retmode=xml"
+    puts url
+    doc = Nokogiri::XML(open(url))
+    doc.xpath('///Grant').each do |node|
+      puts "grant info is:\n\t#{node.text}"
+    end
+  else
+    puts "Needs a pmid to get PubMed info..."
   end
 end
 
 if __FILE__ == $0
-  puts $1
-  get_grant_info($1)
+  puts ARGV[0]
+  get_grant_info(ARGV[0])
 end
